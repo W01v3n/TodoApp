81c30d57dd6baa389c40e305e159fff3a8d98252
wolven <sabadiatal@gmail.com>
2023-04-16 19:45:18 +0300

    make authMiddleware more flexible and reusable for testing and frontend usage

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,8 +1,9 @@
 import jwt, { Secret } from "jsonwebtoken";
-import { Request, Response, NextFunction } from "express";
+import { Response, NextFunction } from "express";
 import AuthRequest from "../interfaces/authRequest.type";
 import DecodedToken from "../interfaces/decodedToken.interface";
 import * as dotenv from "dotenv";
+import { parse } from "cookie";
 dotenv.config();
 
 export default function authMiddleware(
@@ -12,8 +13,20 @@ export default function authMiddleware(
 ) {
   // Extract the token from the Authorization header
   const authHeader = req.headers.authorization;
-  const token =
-    authHeader?.split(" ")[1] || req.body.token || req.headers["token"];
+  const bearerToken = authHeader?.split(" ")[1];
+
+  // Extract the token from the Cookie header
+  const cookieHeader = req.headers.cookie;
+  const cookies = parse(cookieHeader || "");
+  const cookieToken = cookies.token;
+
+  // Extract the token from req.headers["token"]
+  const headerToken = Array.isArray(req.headers["token"])
+    ? req.headers["token"][0]
+    : req.headers["token"];
+
+  // Use the first available token
+  const token = bearerToken || cookieToken || headerToken;
 
   if (!token) {
     return res
@@ -24,7 +37,10 @@ export default function authMiddleware(
   try {
     // Verify the token and extract the user ID
     const jwtSecret = process.env.JWT_SECRET as Secret | undefined;
-    const decoded = jwt.verify(token, jwtSecret ?? "") as DecodedToken;
+    const decoded = jwt.verify(
+      token,
+      jwtSecret ?? ""
+    ) as unknown as DecodedToken;
     req.userId = decoded.userId;
     next();
   } catch (error) {

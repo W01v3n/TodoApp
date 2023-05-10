e54c8abed7552325f479d8c761c89272d2163178
Tal Sabadia <talsa@tau.ac.il>
2023-05-10 08:19:00 +0300

    Changed yellow to blue and styled the button.

--- frontend/src/components/Authenticated/TodoItem.tsx
+++ frontend/src/components/Authenticated/TodoItem.tsx
@@ -13,12 +13,18 @@ function TodoItem({ title, content }: TodoItemProps) {
   }
 
   return (
-    <div>
+    <div className="shadow-md shadow-blue-300">
       <div>
-        <span>{title}</span>
-        <button onClick={toggleContent}>Show more</button>
+        <span>{title} </span>
+        <button
+          className="m-2 bg-blue-400 p-2 opacity-90 shadow-md shadow-blue-500 transition-all duration-150 hover:shadow-lg hover:shadow-blue-500 active:shadow-lg active:shadow-blue-600"
+          onClick={toggleContent}
+        >
+          Open
+        </button>
+
+        {isExpanded && <div>{content}</div>}
       </div>
-      {isExpanded && <div>{content}</div>}
     </div>
   );
 }

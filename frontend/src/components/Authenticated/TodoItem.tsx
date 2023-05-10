6070d12038919df823a243f1543c88e3248a899b
Tal Sabadia <talsa@tau.ac.il>
2023-05-10 08:34:43 +0300

    Added some fixes, better aligned items, and item open transition.

--- frontend/src/components/Authenticated/TodoItem.tsx
+++ frontend/src/components/Authenticated/TodoItem.tsx
@@ -6,15 +6,15 @@ interface TodoItemProps {
 }
 
 function TodoItem({ title, content }: TodoItemProps) {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [isOpen, setisOpen] = useState(false);
 
   function toggleContent() {
-    setIsExpanded(!isExpanded);
+    setisOpen(!isOpen);
   }
 
   return (
-    <div className="shadow-md shadow-blue-300">
-      <div>
+    <div className="py-2 shadow-md shadow-blue-300">
+      <div className="">
         <span>{title} </span>
         <button
           className="m-2 bg-blue-400 p-2 opacity-90 shadow-md shadow-blue-500 transition-all duration-150 hover:shadow-lg hover:shadow-blue-500 active:shadow-lg active:shadow-blue-600"
@@ -22,8 +22,13 @@ function TodoItem({ title, content }: TodoItemProps) {
         >
           Open
         </button>
-
-        {isExpanded && <div>{content}</div>}
+        <div
+          className={`${
+            isOpen ? "max-h-[32rem] opacity-100" : "max-h-0 opacity-0"
+          } overflow-hidden transition-all duration-300 ease-in-out`}
+        >
+          <div>{content}</div>
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/Authenticated/TodoList.tsx
+++ frontend/src/components/Authenticated/TodoList.tsx
@@ -2,11 +2,11 @@ import TodoItem from "./TodoItem";
 
 function TodoList() {
   return (
-    <div className="container mx-28 w-3/6 shadow-lg shadow-blue-900 md:m-0 md:w-auto">
+    <div className="container mx-28 w-3/6 shadow-md shadow-blue-500 md:m-0 md:w-auto">
       <h3 className="bg-blue-300 p-1 text-center text-2xl font-light">
         List Name
       </h3>
-      <ul className="py-4 text-center text-lg ">
+      <ul className="text-center text-lg ">
         <li>
           <TodoItem title="Test Item1" content="This is a content!" />
         </li>

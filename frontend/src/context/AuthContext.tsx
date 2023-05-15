1a29d1afcc7df7e051f0e50fa1a4203a4bedbd65
wolven <sabadiatal@gmail.com>
2023-05-15 16:34:03 +0300

    added explanations for api.ts and AuthContext.tsx

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,13 +1,17 @@
+// Import necessary packages and components.
 import React, { createContext, useState, useEffect, useContext } from "react";
 import { RouteProps } from "react-router-dom";
 import api from "../services/api";
 
+// Define the structure of a User object.
 interface User {
   id: string;
   name: string;
   email: string;
 }
 
+// Define the structure of the authentication context.
+// This context will hold the current user, a loading state, and methods to log in and log out.
 interface AuthContextData {
   currentUser: User | null;
   isLoading: boolean;
@@ -15,12 +19,19 @@ interface AuthContextData {
   logout: () => void;
 }
 
+// Create the context with React's createContext function.
+// This context can hold a value of type AuthContextData, or it can be undefined.
+// Initially, it's set to undefined.
 const AuthContext = createContext<AuthContextData | undefined>(undefined);
 
+// Create a provider component. This component will provide the authentication context to all of its children.
+// This is a common pattern in React: we define a context, then provide it using a special component.
 export const AuthProvider: React.FC = ({ children }: RouteProps) => {
+  // The current user and loading state are held in state variables.
   const [currentUser, setCurrentUser] = useState<User | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
+  // When the component mounts, it makes a request to the /auth/me endpoint to see if there is an authenticated user.
   useEffect(() => {
     const checkAuthenticatedUser = async () => {
       try {
@@ -36,15 +47,21 @@ export const AuthProvider: React.FC = ({ children }: RouteProps) => {
     checkAuthenticatedUser();
   }, []);
 
+  // The login function makes a POST request to the /auth/login endpoint with the email and password.
+  // If the login is successful, it updates the current user.
   const login = async (email: string, password: string) => {
     const response = await api.post("/auth/login", { email, password });
     setCurrentUser(response.data);
   };
 
+  // The logout function makes a POST request to the /auth/logout endpoint.
+  // After the request, it clears the current user.
   const logout = () => {
     api.post("/auth/logout").then(() => setCurrentUser(null));
   };
 
+  // The provider component renders the AuthContext.Provider component with the current user, loading state, and login and logout functions as the value.
+  // All children of this component will have access to these values.
   return (
     <AuthContext.Provider value={{ currentUser, isLoading, login, logout }}>
       {children}
@@ -52,6 +69,8 @@ export const AuthProvider: React.FC = ({ children }: RouteProps) => {
   );
 };
 
+// This is a custom hook that provides an easy way to access the authentication context.
+// It can be used in any functional component that is a child of the AuthProvider.
 export function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined) {
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,39 +1,58 @@
+// Import the axios library, along with two specific types from it: AxiosError and AxiosRequestConfig.
 import axios, { AxiosError, AxiosRequestConfig } from "axios";
 
+// Define a new type that extends the AxiosRequestConfig type. It has an optional property _retry.
 interface AxiosRequestConfigWithRetry extends AxiosRequestConfig {
   _retry?: boolean;
 }
 
+// Create a new axios instance. This instance has some default configuration:
+// it will send requests to "http://localhost:3000/api", and it will include credentials (like cookies) with its requests.
 const instance = axios.create({
   baseURL: "http://localhost:3000/api",
   withCredentials: true,
 });
 
+// Add an interceptor to the axios instance. Interceptors allow you to transform the request or the response in some way.
 instance.interceptors.response.use(
+  // This function will be called when a response is received. It doesn't change the response in any way; it just returns it as-is.
   (response) => response,
+  // This function will be called when an error occurs. It has a chance to recover from certain types of errors.
   async (error: AxiosError) => {
+    // The error object includes the original request configuration. We'll use this to possibly retry the request.
     const originalRequest = error.config as AxiosRequestConfigWithRetry;
+
+    // If the error is a 401 Unauthorized error, and the request was not to the refresh-token or login endpoints,
+    // then we will try to get a new access token and retry the request.
     if (
       error.response?.status === 401 &&
       originalRequest.url !== "/auth/refresh-token" &&
       originalRequest.url !== "/users/login"
     ) {
+      // We only want to retry once, so we use the _retry property to keep track of whether we've already retried this request.
       if (!originalRequest._retry) {
         originalRequest._retry = true;
 
         try {
+          // Attempt to refresh the access token.
           const response = await instance.post("/auth/refresh-token");
+
+          // If the refresh was successful, retry the original request.
           if (response.status === 200) {
             return instance(originalRequest);
           }
         } catch (error) {
+          // If the refresh failed, log the error and give up.
           console.log("Refresh token request failed: ", error);
         }
       }
     }
 
+    // If the error was not a recoverable error, just reject the Promise with the original error.
+    // This will cause the Promise returned by the axios request to be rejected.
     return Promise.reject(error);
   }
 );
 
+// Export the configured axios instance so it can be used elsewhere in the application.
 export default instance;

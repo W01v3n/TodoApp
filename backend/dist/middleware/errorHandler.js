b5ccc5de74b81215c1b61b96e4fb62d42df14e9c
Tal Sabadia <69687310+W01v3n@users.noreply.github.com>
2023-05-20 18:38:35 +0300

    Delete backend/dist directory

--- backend/dist/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const errorHandler = (error, req, res, next) => {
-    console.log(error);
-    res.status(500).json({ message: error.message });
-};
-exports.errorHandler = errorHandler;
--- backend/dist/utils/helpers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isOkPacket = void 0;
-function isOkPacket(data) {
-    return "affectedRows" in data;
-}
-exports.isOkPacket = isOkPacket;
